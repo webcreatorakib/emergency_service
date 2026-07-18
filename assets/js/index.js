67fb8b4ad7698a008948eb5d8fa4bcb90791ad3e
webcreatorakib <webcreatorakib@gmail.com>
2026-07-18 18:38:04 +0600

    Complete Heart Function

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,32 @@
 const cross = document.getElementById('cross');
 const bar = document.getElementById('bar');
 const navItem = document.getElementById('navItem');
-bar.addEventListener('click',function(){
+bar.addEventListener('click', function () {
     bar.style.display = "none";
     cross.style.display = "block";
     navItem.style.display = "block";
 });
-cross.addEventListener('click',function(){
+cross.addEventListener('click', function () {
     cross.style.display = "none";
-    bar.style.display = "block",
+    bar.style.display = "block";
     navItem.style.display = "none";
 })
+// Heart count
+const heartCounts = document.getElementsByClassName('heartCount');
+for (const heartCount of heartCounts) {
+    heartCount.addEventListener('click', () => {
+        const heart = parseInt(document.getElementById('showHeartPoint').innerText);
+        document.getElementById('showHeartPoint').innerText = heart + 1;
+        redHeart();
+    })
+}
+const heart = parseInt(document.getElementById('showHeartPoint').innerText);
+function redHeart (heart){
+    if (heart !== 0) {
+        document.getElementById('redHeart').style.display = "block"
+        document.getElementById('blankHeart').style.display = "none"
+    } else {
+        document.getElementById('blankHeart').style.display = "block"
+        document.getElementById('redHeart').style.display = "none"
+    }
+}
